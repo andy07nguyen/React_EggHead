6a5a8d527e2ec5872deb6896b2f57c9d7014a4d4
Trina Haque <v-trhaqu@microsoft.com>
2017-07-14 16:07:42 -0700

    video 7

--- react-app_nested/src/App.js
+++ react-app_nested/src/App.js
@@ -1,25 +1,8 @@
 import React from 'react';
 
 class App extends React.Component {
-  constructor(){
-    super();
-    // super gives us access to this
-    this.state = {
-      txt: 'this is the state txt',
-      cat: 0
-    }
-  }
-
-  update(e){
-    this.setState({txt: e.target.value})
-  }
   render(){
-    return (
-      <div>
-        <input type="text" onChange={this.update.bind(this)}/>
-        <h2>{this.state.txt} - {this.state.cat}</h2>
-      </div>
-    )
+    return <Button>React</Button>
   }
 }
 
