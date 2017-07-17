c39efeb027bd9da5439403113af176179122d15f
Trina Haque <trina_haque28@outlook.com>
2017-07-16 22:53:29 -0700

    react synthetic event

--- react-app-synthetic-event/src/App.js
+++ react-app-synthetic-event/src/App.js
@@ -4,42 +4,32 @@ class App extends React.Component {
   constructor(){
     super();
     // super gives us access to this
-    this.state = {
-      txt: 'this is the state txt',
-      cat: 0
-    }
+    this.state = {currentEvent: '----'}
+    this.update = this.update.bind(this);
   }
-
   update(e){
-    this.setState({txt: e.target.value})
+    this.setState({currentEvent: e.type})
   }
   render(){
     return (
       <div>
-        <input type="text" onChange={this.update.bind(this)}/>
-        <h2>{this.state.txt} - {this.state.cat}</h2>
+        <textarea
+          onKeyPress = {this.update}
+          onCopy = {this.update}
+          onCut = {this.update}
+          onPaste = {this.update}
+          onFocus = {this.update}
+          onBlur = {this.update}
+          onDoubleClick={this.update}
+          onTouchStart = {this.update}
+          onTouchMove = {this.update}
+          onTouchEnd = {this.update}
+          cols="30"
+          rows="10" />
+        <h2>{this.state.currentEvent}</h2>
       </div>
     )
   }
 }
 
-    // this is dom representation of our component
-    // return React.createElement('h1', null, "i do not want to leave")
-    // 'h1' is an element
-    // null is props
-    // all custom element needs to start with a capital letter
-    // 3rd element can be another element or string that represents element texts
-
-
-// const App = () => <h1>Hi</h1>
-// this is a stateless function component
-
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: "this is the default txt"
-}
 export default App;
